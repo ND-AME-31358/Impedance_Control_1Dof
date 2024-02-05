fb4f2b66d53626b0fb357d8777123d901c977ed3
Shenggao Li <qluanl.sg@gmail.com>
2024-02-05 05:07:49 +0000

    Improve main.cpp comments

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 #include "FastPWM.h"
 #define PI 3.14159265358979323846
 #define PWMfrequency 5000
-#define PWMperiodTicks 12000 // == 60 000 000 / PWMfrequency
+#define PWMperiodTicks 12000 // equals to (60 000 000 / PWMfrequency), and we choose to use 5kHz PWM
 
-// Trick to bypass ISR error
+/* --------------------------------------------------- 
+ * Implementing a workaround to bypass an ISR-related error by substituting 
+   the standard AnalogIn with our custom MyAnalogIn class throughout the experiment. 
+   This change is crucial but the details are not essential for understanding the overall experiment setup.
+   Please ignore the implementation but use MyAnalogIn everytime you want to use AnalogIn
+*/
 class MyAnalogIn : public AnalogIn {
 public:
     MyAnalogIn(PinName inp) : AnalogIn(inp) { }
     virtual void lock() { }
     virtual void unlock() { }
 };
+/* --------------------------------------------------- */
 
 // Define number of communication parameters with matlab
 #define NUM_INPUTS 9
@@ -26,13 +32,16 @@ Timer t;                            // Timer to measure elapsed time of experime
 Ticker currentLoopTicker;           // Ticker to call high frequency current loop
 DigitalOut led_g(LED_GREEN,1);      // UDP server state indicator
 
-/************************Complete the code in this block**************************/
-// Assign digital/analog pins for control and sensing
-FastPWM    M1PWM(D9);               // Motor PWM output (we are using the "FastPWM" rather than built-in "PwmOut" for higher resolution)
-DigitalOut M1INA(D2);               // Motor forward enable
-DigitalOut M1INB(D4);               // Motor backward enable
-MyAnalogIn   CS(A2);                // Current sensor
-/*********************************************************************************/
+/* ===== Complete the code in this block =====
+ * Assign appropriate digital/analog pin number for control and sensing in the 
+ * Example: For digital pin 1, you have to enter D1
+ * Example: For analog  pin 1, you have to enter A1
+*/
+FastPWM    M1PWM(D*);               // Motor PWM output (we are using the "FastPWM" rather than built-in "PwmOut" for higher resolution)
+DigitalOut M1INA(D*);               // Motor forward enable
+DigitalOut M1INB(D*);               // Motor backward enable
+**********    CS(**);               // Current sensor
+/* ===== End of code block =================== */
 
 // Create a quadrature encoder
 // 64(counts/motor rev)*18.75(gear ratio) = 1200(counts/rev)
@@ -45,25 +54,33 @@ const float radPerTick = 2.0*PI/1200.0;
 void setMotorDuty(float duty, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM);
 
 const float SupplyVoltage = 12;     // Supply voltage in Volts
+// Set motor voltage in (V)
 void setMotorVoltage(float voltage, DigitalOut &INA, DigitalOut &INB, FastPWM &PWM);
 
+// Declaration of current current control function.
+// Please refer to the latter part of this file for its definition (implementation).
 void currentLoopFunc();
 
 
 
-float angle = 0.0;
-float velocity = 0.0;
-float voltage = 0.0;
-float current = 0.0;
-float current_error_int = 0.0;
+// Declare global variables
+float angle    = 0.0;    // Motor shaft angle
+float velocity = 0.0;    // Shaft angular velocity
+float voltage  = 0.0;    // Command voltage on motor
+float current  = 0.0;    // Sensed motor current
+float current_error_int = 0.0; // Integrated current error
 
-float current_des   = 0.0f; // Desired current
+float current_des = 0.0; // Desired motor current (a.k.a. command current)
 float Kp   = 0.0; // Kp proportional gain of current control
-float Ki   = 0.0; // Ki integration gain of current control
-float Rm   = 0.0; // Resistance
-float Kb   = 0.0; // Kb (Back EMF)
-float Kv   = 0.0; // Kv friction coe
+float Ki   = 0.0; // Ki integral gain of current control
+float Rm   = 0.0; // Rm Motor Winding Resistance
+float Kb   = 0.0; // Kb Motor Back-EMF constant
+float Kv   = 0.0; // Kv Coefficient of viscous friction
 
+/* Main function that would run on the FRDM board
+ * Note: unlike Arduino, we do not have a setup function and a loop function
+ * The main function would only run once. To have a loop, we have to use while loop by ourselves
+*/ 
 int main (void) {
     // Link the terminal with our server and start it up
     server.attachTerminal(pc);
@@ -77,34 +94,40 @@ int main (void) {
     // Continually get input from MATLAB and run experiments
     float input_params[NUM_INPUTS];
     
+    // infinite while loop, analogous to Arduino's loop function
     while(1) {
         if (server.getParams(input_params,NUM_INPUTS)) {
             // Unpack parameters from MATLAB
             float angle_des = input_params[0]; // Desired angle
-            Rm            = input_params[1]; // Resistance of motor
-            Kb            = input_params[2]; // Kb (Back EMF)
-            Kp            = input_params[3]; // Kp proportional gain of current control
-            Ki            = input_params[4]; // Ki integration gain of current control
-            Kv            = input_params[5]; // Kv friction coe
-            float K       = input_params[6]; // KP of impedance
-            float D       = input_params[7]; // KD of impedance
-            float ExpTime = input_params[8]; // Expriement time in second
+            Rm              = input_params[1]; // Rm Motor Winding Resistance
+            Kb              = input_params[2]; // Kb Motor Back-EMF constant
+            Kp              = input_params[3]; // Kp Proportional gain of current control
+            Ki              = input_params[4]; // Ki Integration gain of current control
+            Kv              = input_params[5]; // Kv Coefficient of viscous friction
+            float K         = input_params[6]; // K  Impedance stiffness
+            float D         = input_params[7]; // D  Impedance damping
+            float ExpTime   = input_params[8]; // Expriement time in second
 
             // Setup experiment
-            t.reset();
-            t.start();
+            t.reset(); // Reset timer
+            t.start(); // Start timer so that we have elapsed time of experiment
             encoder.reset();
-            current_error_int = 0;
-            setMotorVoltage(0,M1INA,M1INB,M1PWM);
-            // Set high frequency corrent loop control
+            current_error_int = 0; // Reset integration of current error
+            setMotorVoltage(0,M1INA,M1INB,M1PWM); //Turn off motor just in case
+            // Set high-frequency (low-level) current control loop
+            // 0.0002 gives the loop period, which means current loop is under 5kHz
             currentLoopTicker.attach(&currentLoopFunc,0.0002);
 
             // Run experiment
             while( t.read() < ExpTime ) { 
 
-                current_des = (-(angle-angle_des)*K -velocity*D)/Kb;
+                /* ===== Complete the code in this block =====
+                 * Impedance control code
+                */
+                current_des = ((angle_des - angle)*K -velocity*D)/Kb;
+                /* ===== End of code block =================== */
 
-                // Form output to send to MATLAB    
+                // Fill the output data to send back to MATLAB
                 float output_data[NUM_OUTPUTS];
                 output_data[0] = t.read();
                 output_data[1] = angle;
@@ -114,29 +137,47 @@ int main (void) {
                 
                 // Send data to MATLAB
                 server.sendData(output_data,NUM_OUTPUTS);
-                wait(.001);                  // Control and sending data in 1kHz
-            }     
+                wait(.001); // Running high-level control loop and sending data in roughly 1kHz
+            } // end of high-level experiment loop
+
             // Cleanup after experiment
             currentLoopTicker.detach();
             server.setExperimentComplete();
             setMotorVoltage(0,M1INA,M1INB,M1PWM);
-        } // end if
-    } // end while
+        } // end if of "check whether we have parameter"
+    } // end while of "infinite while loop"
 } // end main
 
 
+/* Current controller function (low-level control)
+ * This function would be called by the Ticker:currentLoopTicker under 5kHz
+ * This function reads motor state (e.g. angle, current), and set motor voltage
+*/
 void currentLoopFunc(){
-/************************Complete the computation of current sensing***************/
-    // Copy current sensing code from previous part
-    // Read the current sensor value
-    current = 36.7f * (CS - 0.5f);
-/*********************************************************************************/
-    angle = (float)encoder.getPulses()*radPerTick;
-    velocity = encoder.getVelocity()*radPerTick;
-    current_error_int += current - current_des;
-/************************Current Control Code***************/
-    voltage = Rm * current_des + Kb * velocity - Kp*(current - current_des) - Ki*current_error_int;
-/*********************************************************************************/
+    /* ===== Complete the code in this block =====
+     * Copy the current sensing code from your previous project
+     * Important: DO NOT use <float> to re-declare "current" variable again
+     * since we are using the global variable "current"
+    */
+    current  = 0.0f * CS; // Read the current sensor value
+    /* ===== End of code block =================== */
+    
+    // Read angle and velocity from encoder
+    angle    = (float)encoder.getPulses()   * radPerTick; // in rad
+    velocity =        encoder.getVelocity() * radPerTick; // in rad/s
+
+    // Integrate the current error
+    current_error_int += current_des - current;
+
+    /* ===== Complete the code in this block =====
+     * Current controller: compute command voltage on motor
+     * Complete the current controller below from your last project
+     * Note that we have added an integral term to have a PI controller for better performance
+     *
+    *///      |↓↓↓↓↓↓↓↓↓ Copy Your Previous Current Control Code to Here ↓↓↓↓↓↓↓↓| |↓↓↓↓↓ Integral Term ↓↓↓↓|
+    voltage = 0.0 * current_des + 0.0 * velocity + 0.0 * (current_des - current) +  Ki * current_error_int;
+    /* ===== End of code block =================== */
+
     setMotorVoltage(voltage,M1INA,M1INB,M1PWM);
 
 }
