91457ab1db475f71ffb47c9d0fdc720fa8a0306f
Shenggao Li <qluanl.sg@gmail.com>
2024-02-05 00:13:55 -0500

    Improve matlab script comment and readme

--- K64_Impedance_Control_matlab.m
+++ K64_Impedance_Control_matlab.m
@@ -1,4 +1,6 @@
 function output_data = K64_Impedance_Control_matlab()
+%K64_Impedance_Control_matlab Communicate to FRDM board to start impedance controller
+%   See parameters below
     figure(1);  clf;       % Create an empty figure to update later
     subplot(411)
     h1 = plot([0],[0]);
@@ -23,51 +25,46 @@ function output_data = K64_Impedance_Control_matlab()
     % This function will get called any time there is new data from
     % the FRDM board. Data comes in blocks, rather than one at a time.
     function my_callback(new_data)
-        t = new_data(:,1);   % time
-        angle = new_data(:,2); % position
-        vel = new_data(:,3); % velocity
-        voltage = new_data(:,4); % voltage
-        current = new_data(:,5); % current
-        N = length(angle);
-        
-        h1.XData(end+1:end+N) = t;   % Update subplot 1
-        h1.YData(end+1:end+N) = angle;
-        h2.XData(end+1:end+N) = t;   % Update subplot 2
-        h2.YData(end+1:end+N) = vel;
-        h3.XData(end+1:end+N) = t;   % Update subplot 3
-        h3.YData(end+1:end+N) = voltage;
-        h4.XData(end+1:end+N) = t;   % Update subplot 4
-        h4.YData(end+1:end+N) = current;
+      t       = new_data(:,1); % time
+      angle   = new_data(:,2); % position
+      vel     = new_data(:,3); % velocity
+      volt    = new_data(:,4); % voltage
+      current = new_data(:,5); % current
+      N       = length(angle); % number of data points
+      
+      h1.XData(end+1:end+N) = t;   % Update subplot 1
+      h1.YData(end+1:end+N) = angle;
+      h2.XData(end+1:end+N) = t;   % Update subplot 2
+      h2.YData(end+1:end+N) = vel;
+      h3.XData(end+1:end+N) = t;   % Update subplot 3
+      h3.YData(end+1:end+N) = volt;
+      h4.XData(end+1:end+N) = t;   % Update subplot 4
+      h4.YData(end+1:end+N) = current;
     end
     
+    % Setup the communication between PC and FRDM board
     frdm_ip  = '192.168.1.100';     % FRDM board ip
     frdm_port= 11223;               % FRDM board port  
     params.callback = @my_callback; % callback function
     params.timeout  = 2;            % end of experiment timeout
-    
-    % The example program provided takes two arguments
-%     v1 = 1.0;           % pwm applied for first second 
-%     v2 = -1.0;           % pwm applied for second second
-%     input = [v1 v2];    % input sent to FRDM board
-%     output_size = 3;    % number of outputs expected
 
-
+    %% Set experiment parameters
     angle_desire  = 1;      % Desired angle in rad
-    R_motor       = 1.852;	% Resistance of motor
-    Kb            = 0.005;	% Kb (Back EMF)
-    Kp            = 3;      % Kp proportional gain of current control
+    R_motor       = 3.8;    % Winding resistance of motor
+    Kb            = 0.0;    % Motor back-EMF constant
+    Kp            = 3;      % Kp Proportional gain of current control
     Ki            = 0.1;    % Ki Integration gain of current control
-    Kv            = 0.0;    % Kv friction coe
-    K             = 0.01;	% KP of impedance
-    D             = 0.001;	% KD of impedance
+    Kv            = 0.0;    % Kv Coefficient of viscous friction
+    K             = 0.01;	  % K Impedance stiffness
+    D             = 0.001;	% D Impedance damping
     ExpTime       = 3;      % Expriement time in second
     
+    % Pack experiment parameters
     input = [angle_desire R_motor Kb Kp Ki Kv K D ExpTime];
-    output_size = 5;
-   
+    % Number of report data from FRDM
+    output_size = 5; % time, angle, veloticy, voltage, current
+    
+    % Send data to FRDM board, run experiment.
     output_data = RunExperiment(frdm_ip,frdm_port,input,output_size,params);
     
 end
-
-
-
